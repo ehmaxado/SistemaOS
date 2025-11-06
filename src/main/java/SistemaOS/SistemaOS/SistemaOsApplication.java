e11929644c9aae6804df19aeb88248a0c1ccefb3
ehmaxado <eh_maxado@hotmail.com>
2025-11-06 20:30:38 -0300

    first commit

---
+++ src/main/java/SistemaOS/SistemaOS/SistemaOsApplication.java
@@ -0,0 +1,13 @@
+package SistemaOS.SistemaOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaOsApplication.class, args);
+	}
+
+}
